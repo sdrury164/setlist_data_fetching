f2cc3fe8a90019ce2a2822da4344b71868a6d49b
sdrury164 <ubersteve164@gmail.com>
2023-01-06 14:42:14 -0500

    Adding code used to fetch user's developer credentials for Spotify and setlist.fm (placeholders used for actual credentials).

---
+++ get_setlist_fm_api_key.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 30 12:03:14 2022
+
+@author: Steve
+"""
+
+def get_api_key():
+    return 'YOUR-SETLISTFM-API-KEY'
---
+++ get_spotify_credentials.py
@@ -0,0 +1,7 @@
+def get_credential_dict():
+    credential_dict = {
+        'id': 'YOUR-SPOTIFY-ID',
+        'secret': 'YOUR-SPOTIFY-SECRET',
+        'url': 'YOUR-SPOTIFY-URL'
+    }
+    return credential_dict
